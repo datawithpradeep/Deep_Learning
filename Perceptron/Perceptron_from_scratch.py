71deabe80795854cb6b96f37918da6b37d63b8df
datawithpradeep <pradeep.kumar.work01@gmail.com>
2026-08-18 20:21:31 +0530

    Add files via upload

--- Perceptron/Perceptron_from_scratch.py
+++ Perceptron/Perceptron_from_scratch.py
@@ -1,1 +1,27 @@
+# PERCEPTRON FROM SCRATCH
 
+# Step1: Inputs (feature)
+x1=2
+x2=3
+
+
+# Step 2: Weights:
+
+w1=4
+w2=5
+
+# Step 3: Bias
+b=-20
+
+# Step 4: Weighted Sum
+z=(x1*w1)+(x2*w2)+b
+
+print("Weighted sum(z):",z)
+
+# Step 5: Activation Function (Step Function)
+if z>=0:
+    y=1
+else:
+    y=0
+
+print("prediction",y)
